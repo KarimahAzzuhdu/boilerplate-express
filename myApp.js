94fc7d47f7f418830b3ee56d62c9ae28a0a97a0c
Karimah Azzuhdu T.M. <karimah.atm@gmail.com>
2024-11-15 09:35:22 +0000

    completed 50% of the challenges

--- myApp.js
+++ myApp.js
@@ -1,17 +1,30 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use(function middleware(req, res, next){
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+});
 
+console.log("Hello World")
 
+app.get('/', function(req, res){
+    // res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
+})
 
+app.get('/json', (req, res) => {
+    let response;
+    if (process.env.MESSAGE_STYLE === 'uppercase'){
+        response = "HELLO JSON";
+    } else {
+        response = "Hello json";
+    }
+    res.json({"message": response})
+})
 
-
-
-
-
-
-
-
+app.use('/public',express.static(__dirname + '/public'));
 
 
 
